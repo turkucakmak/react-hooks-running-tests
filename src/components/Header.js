eb6b6b2adaf8c2073455b3f16a30907891a596f9
turku <turkucakmak@hotmail.com>
2023-07-06 14:57:46 +0200

    Competed Assignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,8 @@ import React from "react";
 
 function Header(props) {
       return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+        <h1> 
+          {"hello from the Header!"}
         </h1>
       )
 }
